6c415ffb03e24bb25d88a4e749e21ea5a91487f3
a0958822103 <a0958822103@gmail.com>
2025-06-01 22:41:31 +0800

    custom hook

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import useLocalStorage from './custom-hook/uselocalstorage';
 
-function App() {
+export default function App() {
+ 
+  const [name,setName] = useLocalStorage('name','')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input 
+      type = "text"
+      value = {name} 
+      onChange = {e=>setName(e.target.value)}>
+      </input>
+    </>
   );
 }
 
-export default App;
+
